c4613b92cd2f7ef29509dfa60a66bca0ff9cfabb
Michael DeBenedictis <mikedebenedictis@yahoo.com>
2022-04-28 15:46:10 -0400

    added all files

---
+++ src/solution.js
@@ -0,0 +1,7 @@
+/*
+  Write a function that returns a particular student when given a name. If no name matches, return `null`.
+*/
+
+function findStudentByName(students, name) {}
+
+module.exports = findStudentByName;
