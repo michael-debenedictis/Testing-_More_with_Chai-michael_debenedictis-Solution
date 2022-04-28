5502192b683ce086fb2d65c0ec6053a580031045
Michael DeBenedictis <mikedebenedictis@yahoo.com>
2022-04-28 16:09:06 -0400

    created the function and tests

--- src/solution.js
+++ src/solution.js
@@ -2,6 +2,12 @@
   Write a function that returns a particular student when given a name. If no name matches, return `null`.
 */
 
-function findStudentByName(students, name) {}
+function findStudentByName(students, name) {
+  let found = students.find(item => item.name === name);
+  if (found === undefined) {
+    found = null;
+  }
+  return found;
+}
 
 module.exports = findStudentByName;
--- test/solution.test.js
+++ test/solution.test.js
@@ -1,1 +1,28 @@
-// Write your tests here!
+const expect = require("chai").expect;
+
+const findStudentByName = require("../src/solution.js");
+
+describe("findStudentByName", () => {
+    it("should return a matching object to the student name", () => {
+        const students = [
+            { name: "Leo Yeon-Joo", score: 8.9 },
+            { name: "Morgan Sutton", score: 7.4 },
+            { name: "Natalee Vargas", score: 9.2 },
+          ];
+        const actual = findStudentByName(students, 'Natalee Vargas');
+        const expected = { name: 'Natalee Vargas', score: 9.2 };
+        expect(actual).to.eql(expected);
+    })
+})
+
+describe("findStudentByName", () => {
+    it("should return null whenever the name isnt present", () => {
+        const students = [
+            { name: "Leo Yeon-Joo", score: 8.9 },
+            { name: "Morgan Sutton", score: 7.4 },
+            { name: "Natalee Vargas", score: 9.2 },
+          ];
+        const actual = findStudentByName(students, 'Jimmy');
+        expect(actual).to.be.null;
+    })
+})
